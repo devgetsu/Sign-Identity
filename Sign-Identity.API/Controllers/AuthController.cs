04190eef95316e24fef454d14789bda35d0c50d5
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-19 15:42:23 +0500

    REGISTER : Add CreateUser

--- Sign-Identity.API/Controllers/AuthController.cs
+++ Sign-Identity.API/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Sign_Identity.Domain.DTOs;
+using Sign_Identity.Domain.Entities.Auth;
 
 namespace Sign_Identity.API.Controllers
 {
@@ -7,5 +10,61 @@ namespace Sign_Identity.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AuthController(SignInManager<User> signInManager, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterDTO registerDTO)
+        {
+            //check model
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Something went wrong!");
+            }
+            // cehck dto
+            if (string.IsNullOrWhiteSpace(registerDTO.Email))
+            {
+                return BadRequest("Email required");
+            }
+            if (string.IsNullOrWhiteSpace(registerDTO.Username))
+            {
+                return BadRequest("Username required");
+            }
+            if (string.IsNullOrWhiteSpace(registerDTO.FirstName))
+            {
+                return BadRequest("FirstName required");
+            }
+            if (string.IsNullOrWhiteSpace(registerDTO.LastName))
+            {
+                return BadRequest("LastName required");
+            }
+
+            //Mapping
+            var user = new User()
+            {
+                Email = registerDTO.Email,
+                UserName = registerDTO.Username,
+                FirstName = registerDTO.FirstName,
+                LastName = registerDTO.LastName,
+                Age = registerDTO.Age
+            };
+
+            //create user
+            var createdUser = await _userManager.CreateAsync(user, registerDTO.Password);
+
+            if (!createdUser.Succeeded)
+            {
+                return BadRequest("Something went wrong in Create");
+            }
+            return Ok("Congratz");
+        }
     }
 }
--- Sign-Identity.Domain/Entities/Auth/User.cs
+++ Sign-Identity.Domain/Entities/Auth/User.cs
@@ -8,7 +8,7 @@ namespace Sign_Identity.Domain.Entities.Auth
         public string FirstName {  get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
-        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset CreatedDate { get; set; } = DateTimeOffset.UtcNow;
         public DateTimeOffset ModifiedDate { get; set; }
         public DateTimeOffset DeletedDate { get; set; }
         public bool IsDeleted { get; set; }
