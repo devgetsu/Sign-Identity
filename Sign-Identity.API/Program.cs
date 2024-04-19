bee9b60cf2befd21e65ecc26ac576a8c32bf1418
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-19 15:15:57 +0500

    Add project files.

---
+++ Sign-Identity.API/Program.cs
@@ -0,0 +1,54 @@
+
+using Sign_Identity.Infrastructure;
+
+namespace Sign_Identity.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContextOptions(builder.Configuration);
+
+            builder.Services.AddCors(cors =>
+            {
+                cors.AddDefaultPolicy(policy =>
+                {
+                    policy
+                        .AllowAnyHeader()
+                            .AllowAnyOrigin()
+                                .AllowAnyMethod();
+                });
+            });
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseCors();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ Sign-Identity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sign_Identity.Domain.Entities.Auth;
+using Sign_Identity.Infrastructure.Persistance;
+using System.Data;
+using System;
+
+namespace Sign_Identity.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddDbContextOptions(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<SignIdentityDbContext>(options =>
+            {
+                options
+                    .UseLazyLoadingProxies()
+                        .UseNpgsql(configuration.GetConnectionString("Con"));
+            });
+
+            services.AddIdentityCore<User>(options =>
+            {
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireDigit = false;
+            })
+                .AddEntityFrameworkStores<SignIdentityDbContext>();
+
+            return services;
+        }
+    }
+}
